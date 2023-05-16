c351938fffcf79069c457cf100346fe68c03e74a
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2023-05-16 16:27:55 -0300

    feat: requisito 10

--- app/backend/src/middlewares/validateLogin.ts
+++ app/backend/src/middlewares/validateLogin.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 
 const validateLogin = (req: Request, res: Response, next: NextFunction) => {
+  // regex: https://horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
   const { email, password } = req.body;
+  const regex = /\S+@\S+\.\S+/;
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  if (!regex.test(email) || password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   next();
 };
